fc59425741dc13a56322fa428bae1b83913c7c21
alexey_horbunov <algoritm211@gmail.com>
2021-07-11 15:27:34 +0300

    Add classical cors settings

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const userRouter = require('./routes/user.routes')
 
 const app = express()
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors())
 
 app.use(bodyParser.urlencoded({
   extended: true
